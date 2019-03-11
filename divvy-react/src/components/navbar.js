bfc23f0e13f64a4f89882185d631fa8777a18f56
Tom Kowalski <kowalski.tom0@gmail.com>
2019-03-10 23:23:07 -0400

    remove eslint

--- divvy-react/src/components/navbar.js
+++ divvy-react/src/components/navbar.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import { Navbar } from 'react-bootstrap';
+import React, { Component } from 'react'
+import logo from '../logo.svg'
+import { Navbar } from 'react-bootstrap'
 
 class DivvyNav extends Component {
-  render() {
-    return <Navbar bg="light">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-              />
-            <Navbar.Brand>Divvy</Navbar.Brand>
-          </Navbar>;
+  render () {
+    return (
+      <Navbar bg="light">
+        <img
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+          />
+        <Navbar.Brand>Divvy</Navbar.Brand>
+      </Navbar>
+    )
   }
 }
-export default DivvyNav;
-
- 
+export default DivvyNav
