6e7af593030e249ca798d17f7485c43c27c63e6c
l-tn3314 <l.tn3314@gmail.com>
2019-03-10 22:08:00 -0400

    styles for index

--- divvy-react/src/screens/index.js
+++ divvy-react/src/screens/index.js
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom';
 class Index extends React.Component {
   render() {
     return (
-        <div>
+        <div className="screen">
           <h1>Hello, welcome to Divvy</h1>
-          <Link to='/upload'><Button variant="success">Upload</Button></Link>
-          <div>
+          <div className="divvy-upload">
+            <Link to='/upload'><Button className="btn-upload" variant="success" size="lg" block>Upload</Button></Link>
+          </div>
+          <div className="divvy-join">
             <form>
-              <input type="text" name="roomCode" />
+              <input type="text" name="roomCode" className="input-roomcode" placeholder="Room code" />
               <br />
-              <Link to='/room'><Button variant="primary">Submit</Button></Link>
+              <Link to='/room'><Button className="btn-join" variant="primary" size="lg" block>Join</Button></Link>
             </form>
           </div>
         </div>
