380c1f8f01df5b46bae40335a377c66ab5e4e056
Nianmin Guo <zenmai0822@live.com>
2019-04-11 02:55:40 -0400

    Added all endpoints

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -29,12 +29,66 @@ export default class BackendService {
     }
 
     async joinRoom(room) { 
-        return fetch(BackendService.urlPrefix + "/join_room/", {
+        return fetch(BackendService.urlPrefix + "/join_room", {
             method: "post",
             body: JSON.stringify(room),
             headers: {"content-type": "application/json"}
         }).then(response => { 
             response.clone().json();
         })
+    }
+
+    async createItem(item) { 
+        return fetch(BackendService.urlPrefix + "/items", {
+            method: "post", 
+            body: JSON.stringify(item),
+            headers: {"content-type": "application/json"}
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    async updateItem(item) { 
+        return fetch(BackendService.urlPrefix + "/items", {
+            method: "put",
+            body: JSON.stringify(item),
+            headers: {"content-type": "application/json"}
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    async addParticipation(item) { 
+        return fetch(BackendService.urlPrefix + "/amounts", {
+            method: "post", 
+            body: JSON.stringify(item),
+            headers: {"content-type": "application/json"}
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    async updateParticipation(item) { 
+        return fetch(BackendService.urlPrefix + "/amounts", {
+            method: "put",
+            body: JSON.stringify(item),
+            header: {"content-type": "application/json"}
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    async removeParticipation(item) { 
+        return fetch(BackendService.urlPrefix + "/amounts", {
+            method: "delete", 
+            npdu: JSON.stringify(item);
+            header: {"content-type": "application/json"}
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    async getBill(user, roomcode) { 
+        return fetch(BackendService.urlPrefix + `/bill/user/${user}/room/${roomcode}`).then(response => response.json())
     }
 }
