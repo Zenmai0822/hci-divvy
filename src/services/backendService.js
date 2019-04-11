ab2efabf226c6222f233a29fd1a151b92b7d7b9f
Nianmin Guo <zenmai0822@live.com>
2019-04-10 20:14:00 -0500

    Added createNewRoom()

---
+++ src/services/backendService.js
@@ -0,0 +1,32 @@
+export default class BackendService { 
+
+    static urlPrefix = "http://http://www.doublewb.xyz/hci"
+    static instance = null;
+
+    static getInstance() { 
+        if (BackendService.instance === null) { 
+            BackendService.instance = new BackendService()
+        }
+
+        return this.instance;
+    }
+
+    async getRoomInfo(roomCode) { 
+        return fetch(BackendService.urlPrefix + `/rooms/${roomCode}`)
+        .then(response => response.json())
+    }
+
+    async createNewRoom(newRoom) { 
+        return fetch(BackendService.urlPrefix + "/rooms/", {
+            method: "post",
+            body: JSON.stringify(newRoom),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => { 
+            response.clone().json();
+        })
+    }
+
+    
+}
